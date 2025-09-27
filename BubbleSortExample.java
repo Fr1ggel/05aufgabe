ff507523bba84673ff56aab790e73bfac8773a93
Florian <f.pfannenschmidt@outlook.com>
2025-09-27 06:24:22 +0200

    Improved algorithm

--- BubbleSortExample.java
+++ BubbleSortExample.java
@@ -1,15 +1,25 @@
 public class BubbleSortExample {
     static void bubbleSort(int[] arr) {
         int n = arr.length;
-        int temp = 0;
-        for(int i=0; i < n; i++){
-            for(int j=1; j < (n-i); j++){
-                if(arr[j-1] > arr[j]){
-                    //swap elements
-                    temp = arr[j-1];
-                    arr[j-1] = arr[j];
+        int temp;
+        boolean swapped;
+
+        for (int i = 0; i < n; i++) {
+            swapped = false;
+
+            for (int j = 1; j < (n - i); j++) {
+                if (arr[j - 1] > arr[j]) {
+                    // swap elements
+                    temp = arr[j - 1];
+                    arr[j - 1] = arr[j];
                     arr[j] = temp;
+
+                    swapped = true;
                 }
+            }
+
+            if (!swapped) {
+                break;
             }
         }
     }
